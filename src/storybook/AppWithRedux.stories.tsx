f9a38a1df37af078be52512737c1c7630b351ef9
Halina2610 <vievamaxa@gmail.com>
2023-11-05 23:54:17 +0300

    lesson 12/3

--- src/storybook/AppWithRedux.stories.tsx
+++ src/storybook/AppWithRedux.stories.tsx
@@ -1,12 +1,22 @@
-import React from "react";
+import type {Meta, StoryObj} from '@storybook/react';
 import {AppWithRedux} from "../AppWithRedux";
+import {ReduxStoreProviderDecorator} from "./decorators/ReduxStoreProviderDecorator";
 
-export default {
-    title: 'AppWithRedux',
-    component: AppWithRedux
-}
+// More on how to set up stories at:
+// https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta: Meta<typeof AppWithRedux> = {
+    title: 'TODOLISTS/AppWithRedux',
+    component: AppWithRedux,
+    // This component will have an automatically generated Autodocs entry:
+    // https://storybook.js.org/docs/react/writing-docs/autodocs
+    tags: ['autodocs'],
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    decorators: [ReduxStoreProviderDecorator]
+};
 
+export default meta;
+type Story = StoryObj<typeof AppWithRedux>;
 
-export const AppWithReduxBaseExample = () => {
-    return <AppWithRedux />
-}
+// More on component templates:
+// https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+export const AppWithReduxStory: Story = {}
