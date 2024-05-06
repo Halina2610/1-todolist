0212c13a2e26ffb8979024476e4d159935dc35ce
Halina2610 <vievamaxa@gmail.com>
2024-05-06 11:33:56 +0300

    Updates

--- src/api/common.ts..ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const commonTs = axios.create({
-  baseURL: "https://social-network.samuraijs.com/api/1.1/",
-  withCredentials: true,
-  headers: {
-    "API-KEY": "bdcfda08-91c6-49eb-9714-5d59d1951986",
-  },
-});
